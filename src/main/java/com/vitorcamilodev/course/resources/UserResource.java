684a685df3214ee2729272779fe5245ede678f7b
Vitor Camilo <vittor.camilo@gmail.com>
2025-10-10 13:01:53 -0300

    User entity

---
+++ src/main/java/com/vitorcamilodev/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.vitorcamilodev.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vitorcamilodev.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Garmond", "garmond@gmail.com", "123456", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
