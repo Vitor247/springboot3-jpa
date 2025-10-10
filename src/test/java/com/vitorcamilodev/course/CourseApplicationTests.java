898237984b550da4e7ee0ec4dcef753d2fa181d9
Vitor Camilo <vittor.camilo@gmail.com>
2025-10-10 12:58:44 -0300

    Project created

---
+++ src/test/java/com/vitorcamilodev/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorcamilodev.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
